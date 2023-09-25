56e9bad1e12a994fd7ece42ee851f786ab4668f5
Alessandro467 <135671702+Alessandro467@users.noreply.github.com>
2023-09-25 20:16:37 -0300

    Update Programa.java

--- estagio-alessandro/src/main/java/view/Programa.java
+++ estagio-alessandro/src/main/java/view/Programa.java
@@ -5,15 +5,19 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import model.Documento;
+import model.Vaga;
 
 public class Programa {
 	public static void main(String[] args) {
-		Documento d = new Documento(null, "pdf", "");
-		
+		Vaga v = new Vaga(1, "vaga estagio", "Arinaldo",10, "Estagio 2", 111,222);
+				
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto");
 		EntityManager em = emf.createEntityManager();
 		
-		em.close();
-		emf.close();
+		em.getTransaction().begin();
+		em.persist(v);
+
+		em.getTransaction().commit();
+		
 	}
 }
